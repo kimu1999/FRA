a5a3250168ca2f755a606d54efb2ce03cce7bcbd
kimu1999 <39459487+kimu1999@users.noreply.github.com>
2024-11-25 16:19:19 +0900

    Delete main (3).cpp

--- main (3).cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "hello world";
-	cout << "hello world!!"; // À±ÇØº¸
-	cout << "hello world!!!!"; // dlehdrb197
-	cout << "hello world!!!! :)"; //youngjin3407
-}
